ceb53423b54b760ebaef51c497c0f8048efd5c0f
Маша <mbanakova@yandex.ru>
2020-11-08 22:41:40 +0300

    остальные фильтры

--- js/pin.js
+++ js/pin.js
@@ -2,6 +2,7 @@
 (function () {
   const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
   const MAX_SIMILAR_PIN_COUNT = 5;
+  const DEBOUNCE = 500; // 0.5sec
 
   const renderPin = function (offer) {
     if (!offer) {
@@ -42,32 +43,98 @@
     window.data.mapPins.append(pinsFragment);
   };
 
-  let pinz = [];
-  let pinHousingType;
+  let pinsCopy = [];
   const mapFilters = document.querySelector(`.map__filters`);
   const housingTypeOption = mapFilters.querySelector(`#housing-type`);
+  const housingPriceOption = mapFilters.querySelector(`#housing-price`);
+  const housingRoomsOption = mapFilters.querySelector(`#housing-rooms`);
+  const housingGuestsOption = mapFilters.querySelector(`#housing-guests`);
+  const housingFeaturesOption = mapFilters.querySelector(`#housing-features`);
+  const ROOM_PRICE = {
+    low: {
+      min: 0,
+      max: 9999
+    },
+    middle: {
+      min: 10000,
+      max: 49999
+    },
+    high: {
+      min: 50000,
+      max: Infinity
+    }
+  };
 
   const updatePins = function () {
-    if (housingTypeOption.value === `any`) {
-      getPins(pinz, MAX_SIMILAR_PIN_COUNT);
-    }
+    window.main.deletePins();
 
-    const sameTypePins = pinz.filter(function (pin) {
-      return pin.offer.type === pinHousingType;
+    const checkHousingType = function (pin) {
+      if (housingTypeOption.value === `any`) {
+        return true;
+      }
+      return pin.offer.type === housingTypeOption.value;
+    };
+
+    const checkHousingPrice = function (pin) {
+      if (housingPriceOption.value === `any`) {
+        return true;
+      }
+      return pin.offer.price >= ROOM_PRICE[housingPriceOption.value].min && pin.offer.price <= ROOM_PRICE[housingPriceOption.value].max;
+    };
+
+    const checkHousingRooms = function (pin) {
+      if (housingRoomsOption.value === `any`) {
+        return true;
+      }
+      return pin.offer.rooms.toString() === housingRoomsOption.value;
+    };
+
+    const checkHousingGuests = function (pin) {
+      if (housingGuestsOption.value === `any`) {
+        return true;
+      }
+      return pin.offer.guests.toString() === housingGuestsOption.value;
+    };
+
+    const checkHousingFeatures = function (pin) {
+      let housingCheckbox = document.querySelectorAll(`.map__checkbox:checked`);
+
+      return Array.from(housingCheckbox).every(function (feature) {
+        return pin.offer.features.indexOf(feature.value) >= 0;
+      });
+    };
+
+    const pinsFilter = pinsCopy.filter(function (pin) {
+      return checkHousingType(pin) & checkHousingPrice(pin) & checkHousingRooms(pin) & checkHousingGuests(pin) & checkHousingFeatures(pin);
     });
-    getPins(sameTypePins, sameTypePins.length);
+
+    const getPinsNumber = function (pin) {
+      const filteredPins = pin.slice(0, MAX_SIMILAR_PIN_COUNT);
+      return filteredPins;
+    };
+
+    getPinsNumber(pinsFilter);
+
+    getPins(pinsFilter, getPinsNumber(pinsFilter).length);
     window.card.eraseCard();
   };
 
-  housingTypeOption.addEventListener(`change`, function () {
-    window.main.deletePins();
-    const pickedHousingType = housingTypeOption.value;
-    pinHousingType = pickedHousingType;
-    updatePins();
-  });
+  const changeFilterOption = function (filter) {
+    filter.addEventListener(`change`, function () {
+      window.setTimeout(function () {
+        updatePins();
+      }, DEBOUNCE);
+    });
+  };
+
+  changeFilterOption(housingTypeOption);
+  changeFilterOption(housingPriceOption);
+  changeFilterOption(housingRoomsOption);
+  changeFilterOption(housingGuestsOption);
+  changeFilterOption(housingFeaturesOption);
 
   const successPinsLoad = function (data) {
-    pinz = data;
+    pinsCopy = data;
     updatePins();
   };
 
